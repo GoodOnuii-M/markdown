ef754e8f4b9fee920f1275519d64b37fcb049f69
edan <ydu3328@naver.com>
2022-08-01 21:33:24 +0900

    feat: :sparkles: SC-2291 꺽쇠 태그 인라인 파서 추가

---
+++ lib/src/inline_syntaxes/than_syntax.dart
@@ -0,0 +1,24 @@
+import 'package:markdown/src/ast.dart';
+import 'package:markdown/src/inline_parser.dart';
+import 'package:markdown/src/inline_syntaxes/inline_syntax.dart';
+
+class ThanInlineSyntax extends InlineSyntax {
+  ThanInlineSyntax() : super('($_lessThan|$_greaterThan)');
+  static const _lessThan = '&lt';
+  static const _greaterThan = '&lt';
+
+  @override
+  bool onMatch(InlineParser parser, Match match) {
+    final hasMatch = match.groupCount > 0;
+    if (hasMatch) {
+      /// 매칭 문자열
+      final matchedString = match.group(1)!;
+
+      /// Element tex 태그 부착
+      parser.addNode(
+        Text(matchedString == _lessThan ? '<' : '>'),
+      );
+    }
+    return hasMatch;
+  }
+}
