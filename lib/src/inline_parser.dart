6abd986fce60aaa584581708affa20f3387c9a14
edan <ydu3328@naver.com>
2022-08-01 21:49:38 +0900

    refactor: :recycle: SC-2291 디폴트 인라인 파서에 Than 인라인 파서 추가

--- lib/src/inline_parser.dart
+++ lib/src/inline_parser.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:markdown/markdown.dart';
+
 import 'ast.dart';
 import 'charcode.dart';
 import 'document.dart';
@@ -38,6 +40,8 @@ class InlineParser {
     EmphasisSyntax.underscore(),
     CodeSyntax(),
     NonBreakingSpace(),
+    ThanInlineSyntax(),
+
     // We will add the LinkSyntax once we know about the specific link resolver.
   ]);
 
