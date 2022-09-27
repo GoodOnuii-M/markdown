1717c9bce6e02ca8865dac0a43f56a4e03a6dca1
edan <ydu3328@naver.com>
2022-09-27 15:29:25 +0900

    feat: :sparkles: 인라인 속 인라인 (<sub>,<u>,<sup>) 기능 추가 (SC-2664)

--- lib/src/inline_syntaxes/sub_syntax.dart
+++ lib/src/inline_syntaxes/sub_syntax.dart
@@ -1,25 +1,16 @@
-import 'package:markdown/src/ast.dart';
-import 'package:markdown/src/inline_parser.dart';
-import 'package:markdown/src/inline_syntaxes/inline_syntax.dart';
+import 'package:markdown/markdown.dart';
 
-class SubInlineSyntax extends InlineSyntax {
-  SubInlineSyntax() : super('<(?:sub|SUB)>(.*?)</(?:sub|SUB)>');
-
-  @override
-  bool onMatch(InlineParser parser, Match match) {
-    final hasMatch = match.groupCount > 0;
-    if (hasMatch) {
-      /// 매칭 문자열
-      final matchedString = match.group(1)!;
-
-      /// Element tex 태그 부착
-      parser.addNode(
-        Element.text(
-          'sub',
-          matchedString,
-        ),
-      );
-    }
-    return hasMatch;
-  }
+class SubInlineSyntax extends DelimiterSyntax {
+  SubInlineSyntax()
+      : super(
+          '<sub>',
+          requiresDelimiterRun: true,
+          allowIntraWord: true,
+          tags: [
+            DelimiterTag(
+              'sub',
+              1,
+            ),
+          ],
+        );
 }
