6d8ff3e9865d46fb8daaed2611712e4145cbecc7
edan <ydu3328@naver.com>
2022-09-28 14:23:36 +0900

    refactor: :recycle: 하이라이트 신택스 인라인속 인라인 지원하도록 변경

--- lib/src/inline_syntaxes/highlight_syntax.dart
+++ lib/src/inline_syntaxes/highlight_syntax.dart
@@ -1,25 +1,16 @@
-import 'package:markdown/src/ast.dart';
-import 'package:markdown/src/inline_parser.dart';
-import 'package:markdown/src/inline_syntaxes/inline_syntax.dart';
+import 'package:markdown/markdown.dart';
 
-class HighlightInlineSyntax extends InlineSyntax {
-  HighlightInlineSyntax() : super('<(?:mark|MARK)>(.*?)</(?:mark|MARK)>');
-
-  @override
-  bool onMatch(InlineParser parser, Match match) {
-    final hasMatch = match.groupCount > 0;
-    if (hasMatch) {
-      /// 매칭 문자열
-      final matchedString = match.group(1)!;
-
-      /// Element tex 태그 부착
-      parser.addNode(
-        Element.text(
-          'mark',
-          matchedString,
-        ),
-      );
-    }
-    return hasMatch;
-  }
+class HighlightInlineSyntax extends DelimiterSyntax {
+  HighlightInlineSyntax()
+      : super(
+          '<mark>',
+          requiresDelimiterRun: true,
+          allowIntraWord: true,
+          tags: [
+            DelimiterTag(
+              'mark',
+              1,
+            ),
+          ],
+        );
 }
